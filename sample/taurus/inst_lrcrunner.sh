16b8ad31f44e7919d9531289270d96d96f78288e
Wei Sun <wei.sun10@microfocus.com>
2021-04-26 10:43:01 +0800

    update dockerfile to support installation from local package

--- sample/taurus/inst_lrcrunner.sh
+++ sample/taurus/inst_lrcrunner.sh
@@ -1,6 +1,23 @@
 #!/bin/bash
 
-npm i -g --no-save --production https://github.com/weisun10/lrcrunner/releases/download/1.0/lrcrunner-1.0.0.tgz
+# Copyright 2021 - Micro Focus or one of its affiliates
+#
+# The only warranties for products and services of Micro Focus and its affiliates and licensors (“Micro Focus”)
+# are as may be set forth in the express warranty statements accompanying such products and services.
+# Nothing herein should be construed as constituting an additional warranty.
+# Micro Focus shall not be liable for technical or editorial errors or omissions contained herein.
+# The information contained herein is subject to change without notice.
+
+echo "LRC_RUNNER_RELEASE_NUMBER: ${LRC_RUNNER_RELEASE_NUMBER}, LRC_RUNNER_PACKAGE: ${LRC_RUNNER_PACKAGE}"
+
+if [ ! -e "/tmp/${LRC_RUNNER_PACKAGE}" ]; then
+  echo "installing lrcrunner package from github"
+  npm i -g --no-save --production https://github.com/weisun10/lrcrunner/releases/download/${LRC_RUNNER_RELEASE_NUMBER}/${LRC_RUNNER_PACKAGE}
+else
+  echo "installing lrcrunner package from /tmp"
+  npm i -g --no-save --production "/tmp/${LRC_RUNNER_PACKAGE}"
+fi
+
 if [ $? != 0 ]; then
   echo "failed to install lrcrunner"
   exit 1
